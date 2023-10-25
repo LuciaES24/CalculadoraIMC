c5570652a684d1832e5b9b01e3b012dcfbc793b3
LuciaES24 <lespsan543@g.educaand.es>
2023-10-26 00:19:09 +0200

    comenzado IMCResultadoActivity

--- app/src/main/java/com/lespsan543/calculadoraimc/IMCActivity.kt
+++ app/src/main/java/com/lespsan543/calculadoraimc/IMCActivity.kt
@@ -1,12 +1,23 @@
 package com.lespsan543.calculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class IMCActivity : AppCompatActivity() {
+    var resultado = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imcactivity)
     }
 
+
+    fun pulsarCalcular(){
+        //Código para obtener el resultado
+
+
+        val intent = Intent(this,IMCResultadoActivity::class.java)
+        intent.putExtra("resultado",resultado)
+        startActivity(intent)
+    }
 }
--- app/src/main/java/com/lespsan543/calculadoraimc/IMCResultadoActivity.kt
+++ app/src/main/java/com/lespsan543/calculadoraimc/IMCResultadoActivity.kt
@@ -1,11 +1,65 @@
 package com.lespsan543.calculadoraimc
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import java.text.DecimalFormat
 
 class IMCResultadoActivity : AppCompatActivity() {
+    /**
+     * Variables para cada componente del layout
+     */
+    lateinit var mostrarResultado : TextView
+    lateinit var imagenResultado : ImageView
+
+    /**
+     * @param resultadoObtenido double obtenido del activity IMCActivity con el resultado del cálculo
+     */
+    val resultadoObtenido = intent.extras?.getDouble("resultado")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imcresultado)
+
+        inicializarComponentes()
+
     }
+
+    /**
+     * Inicializa cada componente buscandolo por su id en el layout
+     */
+    fun inicializarComponentes(){
+        mostrarResultado = findViewById(R.id.mostrarResultado)
+        imagenResultado = findViewById(R.id.imagenResultado)
+    }
+
+    /**
+     * Muestra el texto con el resultado y la imagen dependiendo de la cifra obtenida
+     */
+    fun mostrarResultadoEImagen(){
+        val resultado = redondearResultado(resultadoObtenido)
+        if (resultado < 18.5){
+
+        }else if(resultado >= 18.5 && resultado<=24.9){
+
+        }else if(resultado >= 25 && resultado<= 29.9){
+
+        }else if(resultado>29.9){
+
+        }
+        mostrarResultado.text = "Tu resultado es de $resultado"
+    }
+}
+
+/**
+ * Redondea cualquier número para que tenga solo 1 decimal
+ *
+ * @param resultado número decimal que queremos redondear
+ * @return double con el número redondeado
+ */
+fun redondearResultado(resultado:Double?) : Double{
+    val formatoDecimal = DecimalFormat("#.#")
+    val cambio = formatoDecimal.format(resultado).toDouble()
+    return cambio
 }
