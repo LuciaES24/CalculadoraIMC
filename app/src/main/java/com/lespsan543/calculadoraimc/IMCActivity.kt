3eb41f450264ee23ff0916f8d35ed7e4653f641e
jorgitorr <jorgitorr97@gmail.com>
2023-10-26 19:39:49 +0200

    nuevos botones

--- app/src/main/java/com/lespsan543/calculadoraimc/IMCActivity.kt
+++ app/src/main/java/com/lespsan543/calculadoraimc/IMCActivity.kt
@@ -17,8 +17,11 @@ class IMCActivity : AppCompatActivity() {
     lateinit var edadNum: Button
     lateinit var edadSum: Button
     lateinit var edadRest: Button
-
+    //genero
     var genero: String = ""
+    lateinit var masculino: Button
+    lateinit var femenino: Button
+
     var resultado = 18.23
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,18 +33,40 @@ class IMCActivity : AppCompatActivity() {
         inicializarListeners()
     }
 
+
+
     fun inicializarComponentes(){
         botonResultado = findViewById(R.id.botonResultado)
         botonAtras = findViewById(R.id.botonAtrasIMC)
 
         //peso
+        pesoNum = findViewById(R.id.textoPesoNumero)
+        pesoSum = findViewById(R.id.boton1Sumar)
+        pesoRest = findViewById(R.id.boton1Restar)
         //altura
-
+        edadNum = findViewById(R.id.textoEdadNumero)
+        edadSum = findViewById(R.id.boton2Sumar)
+        edadRest = findViewById(R.id.boton2Restar)
+        //genero
+        masculino = findViewById(R.id.imagenHombre)
+        femenino = findViewById(R.id.imagenMujer)
     }
+
+    fun
 
     fun inicializarListeners(){
         botonResultado.setOnClickListener { pulsarCalcular() }
         botonAtras.setOnClickListener { onBackPressed() }
+        //peso
+        pesoNum.setOnClickListener {  }
+        pesoSum.setOnClickListener {  }
+        pesoRest.setOnClickListener {  }
+        //edad
+        edadNum.setOnClickListener {  }
+        edadSum.setOnClickListener {  }
+        edadRest.setOnClickListener {  }
+
+
     }
 
     fun pulsarCalcular(){
