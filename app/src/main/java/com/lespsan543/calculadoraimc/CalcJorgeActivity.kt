ea7c1b4812619a1b88374ab5284ab19a47afffac
jorgitorr <jorgitorr97@gmail.com>
2023-10-30 13:05:31 +0100

    calculadora Jorge

--- app/src/main/java/com/lespsan543/calculadoraimc/CalcJorgeActivity.kt
+++ app/src/main/java/com/lespsan543/calculadoraimc/CalcJorgeActivity.kt
@@ -2,10 +2,257 @@ package com.lespsan543.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.jorgearceruiz97.calculadora.CalculoJorge
+import java.text.DecimalFormat
 
 class CalcJorgeActivity : AppCompatActivity() {
+
+    private lateinit var pantalla : TextView
+
+
+    private lateinit var numeros : ArrayList<Button>
+    private lateinit var operaciones : ArrayList<Button>
+
+
+    private lateinit var buttonCE : Button
+    private lateinit var buttonResultado : Button
+    private lateinit var buttonBorrar : Button
+
+
+    private lateinit var calc : CalculoJorge
+    private val df = DecimalFormat("#.##")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calc_jorge)
+
+        calc = CalculoJorge()
+
+
+        //inicializamos los componentes
+        iniciarComponentes()
+        iniciarListeners()
+    }
+
+    /**
+     * Inicializar las variables que se asignarán a cada componente que vamos a controlar.
+     */
+    private fun iniciarComponentes(){
+        pantalla = findViewById(R.id.screen)
+
+        buttonCE = findViewById(R.id.CE)
+        buttonResultado = findViewById(R.id.buttonIgual)
+        buttonBorrar = findViewById(R.id.buttonBorrar)
+
+        pantalla.text = ""
+
+        iniciarBtnNumeros()//inicializa los botones de numeros
+        iniciarBtnOperaciones()//inicializa los botones de operaciones
+    }
+
+    /**
+     * Establecer las variables del ArraList numeros que controlarán los dígitos y el punto decimal.
+     */
+    private fun iniciarBtnNumeros(){
+        numeros = ArrayList()
+        numeros.add(findViewById(R.id.button0))
+        numeros.add(findViewById(R.id.button1))
+        numeros.add(findViewById(R.id.button2))
+        numeros.add(findViewById(R.id.button3))
+        numeros.add(findViewById(R.id.button4))
+        numeros.add(findViewById(R.id.button5))
+        numeros.add(findViewById(R.id.button6))
+        numeros.add(findViewById(R.id.button7))
+        numeros.add(findViewById(R.id.button8))
+        numeros.add(findViewById(R.id.button9))
+        numeros.add(findViewById(R.id.buttonDecimal))
+    }
+
+    /**
+     * Guarda las variables en el ArrayList btnOperaciones que realizan los calculos
+     */
+    private fun iniciarBtnOperaciones(){
+        operaciones = ArrayList()
+        operaciones.add(findViewById(R.id.buttonSum))
+        operaciones.add(findViewById(R.id.buttonRest))
+        operaciones.add(findViewById(R.id.buttonMult))
+        operaciones.add(findViewById(R.id.buttonDiv))
+    }
+
+    /**
+     * Establecer los eventos y las funciones asociadas de cada componente que vamos a controlar.
+     */
+    private fun iniciarListeners(){
+        for (i in 0..<numeros.count()){
+            numeros[i].setOnClickListener{ btnNumClicked(i) }
+        }
+
+        for (i in 0..<operaciones.count()){
+            operaciones[i].setOnClickListener{ btnOperClicked(i) }
+        }
+
+        buttonCE.setOnClickListener{ btnCEClicked() }
+        buttonResultado.setOnClickListener{ btnResultClicked() }
+        buttonBorrar.setOnClickListener{btnBorrarClicked()}
+
+    }
+
+    /**
+     * Metodo que borra el número que se está introduciendo
+     * Primero comprobamos que numero es el que se esta introduciendo o está por pantalla con el booleano
+     * calc.primerNum se comprueba eso, luego modificamos el número
+     */
+    private fun btnBorrarClicked() {
+        if(calc.primerNum){
+            if(calc.numTemp1.isNotEmpty()){//si el numTemp 1 no está vacío
+                calc.numTemp1 = calc.numTemp1.substring(0,calc.numTemp1.length-1) //le quita un digito al num
+                muestraValor(calc.numTemp1,calc.numTemp1) //muestra en pantalla y en detalle el numero alterado
+            }else{
+                mensajeError("No existe nada para borrar")
+            }
+        }else{
+            if(calc.numTemp2.isNotEmpty()){
+                calc.numTemp2 = calc.numTemp2.substring(0,calc.numTemp2.length-1)
+                muestraValor(calc.numTemp2,calc.numTemp1 + calc.operadorTxt() + calc.numTemp2)
+            } else if (calc.operacion != 0) {
+                calc.operacion = 0 //si el calc.op es diferente a cero lo iguala a 0
+                muestraValor(calc.numTemp1, calc.numTemp1) // muestra el valor en la pantalla y el detalle de numTemp1
+            } else {
+                calc.primerNum = true // si el numTemp2 está vacio y ademas el operador el 0 cambia a true el primerNum y entonces va al primer if de cal.primerNum = true
+                muestraValor(calc.numTemp1, calc.numTemp1) //muestra el valor en la pantalla y el detalle de numTemp1
+            }
+        }
+    }
+
+    /**
+     * Agrega el dígito pulsado en el número correspondiente del objeto calc.
+     *
+     *
+     * @param num dígito pulsado del 0 al 9 o punto decimal (10)
+     */
+    private fun btnNumClicked(num : Int){
+        calc.tecleaDigito(num)
+
+        //Mostramos info actualizada en los TextView de la app
+        if (calc.primerNum) {
+            muestraValor(calc.numTemp1, calc.numTemp1)
+        }
+        else {
+            muestraValor(calc.numTemp2, calc.numTemp1 + calc.operadorTxt() + calc.numTemp2)
+        }
+    }
+
+    /**
+     * Agrega la operación del cálculo a realizar
+     *
+     * @param num operación (0 -> + / 1 -> - / 2 -> * / 3 -> /
+     */
+    private fun btnOperClicked(num : Int){
+        if (calc.primerNum) {
+            //Tratamiento de la operación cuando estamos introduciendo el primer número.
+
+            if (calc.numCalculos > 0 && calc.numTemp1 == "") {
+                //Si hay un cálculo anterior y el num1 aún está vacío, el resultado anterior es el num1 del siguiente cálculo.
+                calc.num1 = calc.resultado.toFloat()
+                calc.numTemp1 = df.format(calc.resultado).toString()
+            }
+            else {
+                //Sino, asignamos num1 del objeto calc convirtiendo los dígitos introducidos a float.
+                //Además, si existe algún problema o cuando si se pulsa un operador sin introducir número antes, lo capturamos y usamos el valor 0.
+                try {
+                    calc.num1 = calc.numTemp1.toFloat().toFloat()
+                } catch (e: NumberFormatException) {
+                    calc.num1 = 0.0F
+                    calc.numTemp1 = "0"
+                }
+            }
+
+            //Asignamos el operador al objeto calc, mostramos info en pantalla y actualizamos las características necesarias de calc para indicar que pasamos al estado de introducir el segundo número.
+            calc.operacion = num
+            muestraValor(calc.operadorTxt(), calc.numTemp1 + calc.operadorTxt())
+            calc.numTemp2 = ""
+            calc.primerNum = false
+        }
+        else if (calc.numTemp2 == "") {
+            //Si se introduce una operación y aún no existe el segundo número la nueva operación debe reemplazar la operación anterior.
+
+            calc.operacion = num
+            //Mostramos en pantalla la actualización del operador.
+            muestraValor(calc.operadorTxt(), calc.numTemp1 + calc.operadorTxt())
+        }
+        else {
+            //Tratamiento de la operación cuando estamos introduciendo el segundo número.
+
+            //Convertimos la cadena de dígitos en el número 2 y realizamos el cálculo.
+            //Si existe algún problema en la conversión la controlamos asignando el valor 0.
+            calc.num2 = try { calc.numTemp2.toFloat() } catch (e: NumberFormatException) { 0f }
+            calc.calcular()
+
+            //Mostramos en pantalla el resultado del cálculo como detalle y la operación en la pantalla principal.
+            muestraValor(calc.operadorTxt(num), df.format(calc.resultado).toString() + calc.operadorTxt(num))
+
+            //Actualizamos las características necesarias del objeto calc, ya que vamos a seguir en el estado de introducir solo un segundo número, ya que el primer número y la operación es asignado como el resultado del cálculo realizado y la nueva operación introducida.
+            calc.num1 = calc.resultado
+            calc.operacion = num
+            calc.num2 = 0f
+            calc.numTemp1 = df.format(calc.num1).toString()
+            calc.numTemp2 = ""
+        }
+    }
+
+    /**
+     * Reiniciar las características del objeto calc cuando se pulsa el botón CE
+     */
+    private fun btnCEClicked() {
+        //Mostramos en pantalla y detalle la cadena de caracteres vacía.
+        muestraValor("", "")
+
+        //Inicializamos las características del objeto calc.
+        calc.iniValores()
+    }
+
+    /**
+     * Acciones a realizar al pulsar el botón =.
+     * Solo se ejecutará si estamos introduciendo el segundo número.
+     * Realizará las acciones necesarias para mostrar el cálculo en pantalla.
+     */
+    private fun btnResultClicked(){
+        if (!calc.primerNum && calc.numTemp2 != ""){
+            //Si estamos introduciendo el segundo número, lo actualizamos convirtiendo la cadena de dígitos y calculamos la operación.
+            calc.num2 = try { calc.numTemp2.toFloat() } catch (e: NumberFormatException) { 0f }
+            calc.calcular()
+
+            //Mostramos en pantalla el resultado y en detalle toda la operación (num1 + num2 = result) formateando a 2 posiciones decimales.
+            muestraValor(df.format(calc.resultado).toString(), df.format(calc.num1).toString() + calc.operadorTxt() + df.format(calc.num2).toString() + "=" + df.format(calc.resultado).toString())
+
+            //Inicializamos las características del objeto calc, excepto el núemro de cálculos.
+            calc.iniValores(resetNumCalculos = false, resetResult = false)
+        }
+        else {
+            mensajeError("Debe introducir 2 números y una operación para mostrar un resultado")
+        }
+    }
+
+    /**
+     * Muestra la información en los componentest TextView txtPantalla y txtDetalle.
+     *
+     * @param pantalla info a mostrar en txtPantalla
+     * @param detalle info a mostrar en txtDetalle
+     */
+    private fun muestraValor(pantalla : String, detalle : String){
+        this.pantalla.text = getString(R.string.pantalla, pantalla)
+    }
+
+
+    /**
+     * Muestra un mensaje de error en pantalla durante un tiempo corto.
+     *
+     * @msj mensaje de error
+     */
+    private fun mensajeError(msj: String) {
+        Toast.makeText(this, msj, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/lespsan543/calculadoraimc/CalculoJorge.kt
+++ app/src/main/java/com/lespsan543/calculadoraimc/CalculoJorge.kt
@@ -1,100 +1,90 @@
 package com.jorgearceruiz97.calculadora
 
 class CalculoJorge {
-    //los atributos en kotlin no tiene get y set
-    var num1 = 0
-    var num2 = 0
-    var resultado = 0
-    var operacion = 0
 
-    var primerNum = true // true -> esperando num1 / false -> esperando num2
-    var num1Temp = ""
-    var num2Temp = ""
-    var numCalculos = 0
+    var num1: Float = 0f
+    var num2: Float = 0f
+    var operacion: Int = 0
+    var resultado: Float = 0f
 
+
+    var primerNum: Boolean = true // true -> Esperando num1 / false -> Esperando num2
+    var numTemp1: String = ""
+    var numTemp2: String = ""
+    var numCalculos: Int = 0
 
     /**
-     * realiza la llamada al método adecuado para realizar el cálculo solicitado a una calculadora
-     * si el num1Temp y el num2Temp no están vacios los cambia a enteros y realiza una operación
-     * segun el número que tenga almacenado
-     * si no el resultado es cero
+     * Realiza la llamada al método adecuado para realizar el cálculo solicitado en la calculadora.
      */
-    fun calcular(){
-        if (num1Temp.isNotEmpty() && num2Temp.isNotEmpty()) {
-            num1 = num1Temp.toInt()
-            num2 = num2Temp.toInt()
-
-            when (operacion) {
-                0 -> suma()
-                1 -> resta()
-                2 -> mult()
-                3 -> div()
-            }
-            numCalculos++
-        } else {
-            resultado = -1 //no te devuelve ninguna operacion
+    fun calcular() {
+        when (this.operacion) {
+            0 -> this.suma()
+            1 -> this.resta()
+            2 -> this.multiplica()
+            3 -> this.divide()
         }
+        this.numCalculos += 1
     }
 
     /**
-     * resetea el primerNum a true y los num1 y 2 y numeros temporales
+     * Realiza la suma de num1 y num2 y lo almacena en result.
      */
-    fun resetear(){
-        primerNum = true
-        num1 = 0
-        num2 = 0
-        num1Temp = ""
-        num2Temp = ""
-    }
-
-    /**
-     * establece numero temporal, si el primerNum ha sido introducido se modificara el segundo numTemporal
-     * @param numero que se va a añadir como numero temporal
-     */
-    fun establecerNumero(numero: Int) {
-        if (primerNum) {
-            num1Temp += numero
-        } else {
-            num2Temp += numero
-        }
-    }
-
-    /**
-     * establece la operacion, cambia el primer num a false ya que se prevee que una vez
-     * se introduzca una operación el siguiente num será el num2
-     * @param op equivale a la operacion que va a realizar el usuario
-     */
-    fun establecerOperacion(op: Int) {
-        operacion = op
-        primerNum = false
-    }
-
     private fun suma() {
-        resultado = num1 + num2
+        this.resultado = this.num1 + this.num2
     }
 
+    /**
+     * Realiza la resta de num1 y num2 y lo almacena en result
+     */
     private fun resta() {
-        resultado = num1 - num2
+        this.resultado = this.num1 - this.num2
     }
 
-    private fun mult() {
-        resultado = num1 * num2
+    /**
+     * Realiza la multiplicación de num1 y num2 y lo almacena en result
+     */
+    private fun multiplica() {
+        this.resultado = this.num1 * this.num2
     }
 
-    private fun div() {
-        resultado = if (num2 != 0) {
-            num1 / num2
-        } else {
-            0
+    /**
+     * Realiza la división de num1 y num2 y lo almacena en result
+     */
+    private fun divide() {
+        this.resultado = this.num1 / this.num2
+    }
+
+    /**
+     * Según el parámetro num, retornará el símbolo correspondiente a la operación
+     *
+     * @param num operación (0 -> + / 1 -> - / 2 -> * / 3 -> /
+     * @return String con el símbolo de la operación
+     */
+    fun operadorTxt(num: Int): String {
+        return when (num) {
+            0 -> "+"
+            1 -> "-"
+            2 -> "*"
+            3 -> "/"
+            4 -> "<-"
+            else -> ""
         }
     }
 
     /**
-     * obtiene el resultado
-     * @return resultado que se obtiene al realizar una operación
+     * Según this.op, retornará el símbolo correspondiente a la operación
+     *
+     * @return String con el símbolo de la operación
      */
-    fun obtenerResultado(): Int {
-        return resultado
+    fun operadorTxt(): String {
+        return when (this.operacion) {
+            0 -> "+"
+            1 -> "-"
+            2 -> "*"
+            3 -> "/"
+            4 -> "<-"
+            else -> ""
+        }
     }
 
     /**
@@ -106,23 +96,39 @@ class CalculoJorge {
         //Si es menor que 10, se trata de un dígito del 0 al 9.
         //Sino, es el punto decimal.
         if (num < 10){
-            if (this.primerNum) this.num1Temp += num.toString()
-            else this.num2Temp += num.toString()
+            if (this.primerNum) this.numTemp1 += num.toString()
+            else this.numTemp2 += num.toString()
         }
         else {
             if (this.primerNum) {
                 //Si no se tecleo ningún dígito antes del punto decimal, establecemos el valor 0.
                 //Sino, lo agregamos a la cadena siempre que no exista ya con anterioridad
-                if (this.num1Temp == "") this.num1Temp = "0."
-                else this.num1Temp += if (this.num1Temp.contains('.')) "" else "."
+                if (this.numTemp1 == "") this.numTemp1 = "0."
+                else this.numTemp1 += if (this.numTemp1.contains('.')) "" else "."
             }
             else {
                 //Mismas acciones, pero con la cadena que va recogiendo el número 2
-                if (this.num2Temp == "") this.num2Temp = "0."
-                else this.num2Temp += if (this.num2Temp.contains('.')) "" else "."
+                if (this.numTemp2 == "") this.numTemp2 = "0."
+                else this.numTemp2 += if (this.numTemp2.contains('.')) "" else "."
             }
 
         }
+    }
+
+    /**
+     * Inicializar las características del objeto calc.
+     *
+     * @param resetNumCalculos indica si ponemos a 0 o no el valor de numCalculos
+     */
+    fun iniValores(resetNumCalculos: Boolean = true, resetResult: Boolean = true){
+        this.num1 = 0f
+        this.num2 = 0f
+        this.operacion = 0
+        if (resetResult) this.resultado = 0f
+        this.primerNum = true
+        this.numTemp1 = ""
+        this.numTemp2 = ""
+        if (resetNumCalculos) this.numCalculos = 0
     }
 
 }
